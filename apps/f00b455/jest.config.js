acd5d0d269c53bf2bf99a3048beb62bbebd6415b
f00b455 <haumic@gmail.com>
2021-11-23 23:24:11 +0100

    initial commit

---
+++ apps/f00b455/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'f00b455',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/f00b455',
+};
